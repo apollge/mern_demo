d43956e4f9fe07548c52958a3945972da73a2041
Jan G <apollg3@gmail.com>
2019-07-29 17:22:53 +0800

    login / logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,10 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -25,7 +28,7 @@ export default function(state = initialState, action) {
       };
 
     case REGISTER_SUCCESS:
-    case AUTH_ERROR:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -35,6 +38,9 @@ export default function(state = initialState, action) {
       };
 
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
